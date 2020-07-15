178dda1e63c0822780d0b21bcc561e1313ec6b3c
Melvisharam info <mvsweb@users.noreply.github.com>
2020-07-15 18:16:00 +0530

    SVT INFRA UPDATE

--- contact-mail.php
+++ contact-mail.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hifzurwebdeveloper@gmail.com";
+        $recipient = "maaz.aws@gmail.com";
         
 
         // Set the email subject.
